92b7cef6c3510ac6a130ece5fa774b9642a09362
firdaushassansalim <firdaushassan033@gmail.com>
2021-10-06 12:08:27 +0300

    worked satisfactorily with classes, objects and instances

--- 059/employee.py
+++ 059/employee.py
@@ -4,13 +4,35 @@
 class Employee:
     """Represents an employee"""
 
-    def __init__(self, first_name, last_name, age):
+    total_employees = 0
+    raise_amount = 1.10
+
+    def __init__(self, first_name, last_name, pay):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
+        self.pay = pay
+
+        Employee.total_employees += 1
+
+    def print_names(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def raise_pay(self):
+        return (self.pay ** Employee.raise_amount)
+
+    @classmethod
+    def raise_salary(cls, amount):
+        cls.raise_amount = amount
 
 person_one = Employee('Caleb', 'Curry', 27)
 person_two = Employee('Ali', 'Abdaal', 25)
 
-print(person_one.first_name, '\t', person_one.last_name, '\t', person_one.age)
-print(person_two.first_name, '\t', person_two.last_name, '\t', person_two.age)
+print(person_one.print_names())
+print(person_two.print_names())
+
+Employee.raise_salary(120)
+
+print(person_one.raise_amount)
+print(person_two.raise_pay())
+
+print(Employee.total_employees)
