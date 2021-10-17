5cb35783be4f5b6d5753970af115fecfa6f56c6e
firdaushassansalim <firdaushassan033@gmail.com>
2021-10-17 18:33:18 +0300

    working with lists

---
+++ 070/working-with-lists/magician.py
@@ -0,0 +1,4 @@
+magicians = ["firdaus", "collins", "amos", "tesfedze"]
+for magician in  magicians:
+	print("{}, nice trick there.\nI am looking forward to your next trick, {}\n".format(magician.title(), magician.title()))
+print("Thank you all for showing us your magic.\n")
---
+++ 070/working-with-lists/pizza.py
@@ -0,0 +1,5 @@
+pizzas = ["meat-pie", "potato-pie", "pork-pie", "pan-cake"]
+
+for pizza in pizzas:
+      print("I like {}".format(pizza.title()))
+print("\nI like pizza too much.\n")
