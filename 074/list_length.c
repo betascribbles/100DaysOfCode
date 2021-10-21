d28e1d03cc3a7c54ccffb99f0f3fd1208559a15b
firdaushassansalim <firdaushassan033@gmail.com>
2021-10-21 09:40:04 +0300

    return the length of a linked list

---
+++ 074/list_length.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/* Struct used */
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+/* Prints the length of a list */
+size_t list_len(const list_t *h)
+{
+      size_t list_len = 0;
+
+      while (h != NULL)
+      {
+            h = h->next;
+            list_len++;
+      }
+      return list_len;
+}
+
+/* START OF PROGRAM */
+int main(void)
+{
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = list_len(head);
+    printf("-> %lu elements\n", n);
+    free(new->str);
+    free(new);
+    return (0);
+}
